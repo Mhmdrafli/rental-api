7914e1285137c44f86d13823ff7c3327215bcd96
Mhmdraflyy <raflimuhammad646@gmail.com>
2026-04-19 20:11:58 +0700

    setup cors

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080'],
+    'allowed_origins' => [
 
+    'https://auto-rent-express.vercel.app', 
+
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
